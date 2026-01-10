c033783b19ca4348723ddf95c05264a7b7b3b93c
gutembergp-droid <gutembergp@gmail.com>
2026-01-09 22:02:41 -0300

    conexao

--- .next/dev/static/chunks/Documents_v0-erp-obra-principal-verce_app_layout_tsx_15fd0332._.js
+++ .next/dev/static/chunks/Documents_v0-erp-obra-principal-verce_app_layout_tsx_15fd0332._.js
@@ -2,5 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__10f71ca2._.css",
-  "static/chunks/de892_de5a6f6c._.js",
-  "static/chunks/Documents_v0-erp-obra-principal-verce_052e5
+  "static/chunks/Documents_v0-erp-obra-principal-verce_7b434e6d._.js"
+],
+    source: "dynamic"
+});
